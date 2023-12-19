3a5bb8efc87737f58394acc6a03a46e75e18d42a
Pizarman13 <dpizagar@myuax.com>
2023-12-19 01:36:56 +0100

    Archivos

---
+++ archivos.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Pizarman on 19/12/2023.
+//
+
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("file.txt", "w");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
+
+    fprintf(file, "Hola, mundo!\n");
+
+    // Cerrar el archivo después de escribir
+    fclose(file);
+
+    // Abrir de nuevo para leer
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    fscanf(file, "%s", buffer);
+
+    printf("Leido del archivo: %s\n", buffer);
+
+    // Cerrar el archivo después de leer
+    fclose(file);
+
+    return 0;
+}
